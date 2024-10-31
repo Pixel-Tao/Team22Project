a22d4c801a1a16f251295df6b686606cea865ad4
Tao <62868403+Pixel-Tao@users.noreply.github.com>
2024-10-31 20:22:35 +0900

    [Add] TileSO 추가, BuildSO 추가, TileObject 추가

---
+++ Assets/ScriptableObjects/BuildSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "S0_BuildData", menuName = "Datas/S0_BuildData")]
+public class BuildSO : ScriptableObject
+{
+    public string displayName;
+    public string description;
+}
