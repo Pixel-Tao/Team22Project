bf9433c0e325b63b680e07cdf1470b31f4baa49d
Tao <62868403+Pixel-Tao@users.noreply.github.com>
2024-11-01 16:56:15 +0900

    [Add] PoolManager 기능 추가

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class GameScene : SceneBase
 {
+    public int poolDefaultCapacity = 20;
+    public int poolMaxSize = 100;
+
     protected override void OnSceneLoad()
     {
         Debug.Log("GameScene OnSceneLoad");
+        PoolManager.Instance.Init(poolDefaultCapacity, poolMaxSize);
         UIManager.Instance.ShowSceneUI<GameSceneUI>();
         UIManager.Instance.ShowPopupUI<InventoryPopupUI>();
         UIManager.Instance.CloseAllPopupUI();
