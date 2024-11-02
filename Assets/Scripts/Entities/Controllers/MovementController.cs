f2c305774094450afd68c7dd9dbcb9a563fcf8cf
Tao <62868403+Pixel-Tao@users.noreply.github.com>
2024-11-02 21:24:45 +0900

    [Fix] 에러 수정

--- Assets/Scripts/Entities/Controllers/MovementController.cs
+++ Assets/Scripts/Entities/Controllers/MovementController.cs
@@ -1,6 +1,5 @@
+using Defines;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class MovementController : MonoBehaviour
 {
@@ -40,18 +39,14 @@ public class MovementController : MonoBehaviour
     }
     private void ApplyMove(Vector3 dir)
     {
-        CharacterMoveStepType type = CharacterMoveStepType.Idle;
+        CharacterMoveStepType type = CharacterMoveStepType.Forward;
         float forwardDot = Vector3.Dot(transform.forward, moveDirection);
         float rightDot = Vector3.Dot(transform.right, moveDirection);
         // 화면에서 볼때 캐릭터가 아래로 보면 z축이 -1, 위로 보면 z축이 1, 왼쪽으로 보면 x축이 -1, 오른쪽으로 보면 x축이 1 이다.
         // 이동 방향이 위쪽(즉 z 축이 1)이고 캐릭터가 바라보는 방향아 아래쪽일 때, 캐릭터는 뒷걸음질 치는 모션을 취해야하고, 캐릭터가 바라보는 방향으로 이동 할때는 전진하는 모션을 취해야하고, 바라보는 방향에서 좌, 우로 움직일때는 사이드 스텝으로 모션을 취해야한다.
+        // 양 방향간의 내적으로 캐릭터의 움직임을 정해 애니메이션을 취하도록 한다.
         float maxMoveSpeed = moveSpeed;
-        if (forwardDot > 0.5)
-        {
-            maxMoveSpeed = moveSpeed;
-            type = CharacterMoveStepType.Forward;
-        }
-        else if (forwardDot < -0.5)
+        if (forwardDot < -0.5)
         {
             maxMoveSpeed = moveSpeed * 0.5f;
             type = CharacterMoveStepType.Backward;
