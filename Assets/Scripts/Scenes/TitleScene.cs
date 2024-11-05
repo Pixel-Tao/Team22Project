fe022790e0601aac7f29959e25bccef60410a43a
Tao <62868403+Pixel-Tao@users.noreply.github.com>
2024-11-05 23:35:00 +0900

    [Add] TilteScene 준비

--- Assets/Scripts/Scenes/TitleScene.cs
+++ Assets/Scripts/Scenes/TitleScene.cs
@@ -5,6 +5,12 @@ public class TitleScene : SceneBase
     protected override void OnSceneLoad()
     {
         Debug.Log("TitleScene OnSceneLoad");
+
+        UIManager.Instance.ShowSceneUI<TitleSceneUI>();
+        GameObject mapParent = new GameObject("MapParent");
+        mapParent.transform.position = Vector3.zero;
+        GameObject map = ResourceManager.Instance.Instantiate("WorldMap", mapParent.transform);
+        map.transform.localPosition = Vector3.zero;
     }
 
     protected override void OnSceneUnloaded()
--- Assets/Scripts/UIs/Scene/TitleSceneUI.cs
+++ Assets/Scripts/UIs/Scene/TitleSceneUI.cs
@@ -1,5 +1,24 @@
 
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class TitleSceneUI : UIScene
 {
     // 타이틀씬에서 사용할 고정 UI
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OptionButton()
+    {
+        // TODO : 설정
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
 }
