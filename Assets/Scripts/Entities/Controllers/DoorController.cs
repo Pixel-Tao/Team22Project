4e54978ab1a9bf00bf1a3fda4d243e98fe2249b5
Tao <62868403+Pixel-Tao@users.noreply.github.com>
2024-11-06 18:03:04 +0900

    [Update] 성문 열리게, 마켓 빵생성, 마켓 건설

---
+++ Assets/Scripts/Entities/Controllers/DoorController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] private Transform leftDoor;
+    [SerializeField] private float leftDoorOpenAngle = 90f;
+
+    [SerializeField] private Transform rightDoor;
+    [SerializeField] private float rightDoorOpenAngle = -90f;
+
+    public void Open()
+    {
+        if (leftDoor == null) return;
+        leftDoor.localRotation = Quaternion.Euler(0, leftDoorOpenAngle, 0);
+        rightDoor.localRotation = Quaternion.Euler(0, rightDoorOpenAngle, 0);
+    }
+
+    public void Close()
+    {
+        leftDoor.localRotation = Quaternion.identity;
+        rightDoor.localRotation = Quaternion.identity;
+    }
+}
